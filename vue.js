0e28e1d075af49f4ad740d8eb9da09f40c0a2689
Marco Giammari <marcogiammari@gmail.com>
2023-07-29 16:52:20 +0200

    single-page, background

--- vue.js
+++ vue.js
@@ -5,6 +5,7 @@ createApp({
         return {
             database: {},
             api: 'https://countriesnow.space/api/v0.1/countries/capital',
+            gameStarted: false,
             opzioni: [],
             randomNum: null,
             questionsTotal: 5,
@@ -19,44 +20,44 @@ createApp({
     methods: {
         sendData() {
             axios.post('capitali.php', this.database, {
-                headers: { 'Content-Type': 'multipart/form-data'}
+                headers: { 'Content-Type': 'multipart/form-data' }
             })
-            .then(risp => {
-                this.opzioni = risp.data;
-                this.randomNum = Math.floor(Math.random() * 4);
-                this.domanda = this.opzioni[this.randomNum].name;
-            })
-            .catch(error => {
-                console.log("Errore nell'inviare i dati: ", error);
-            })
+                .then(risp => {
+                    this.opzioni = risp.data;
+                    this.randomNum = Math.floor(Math.random() * 4);
+                    this.domanda = this.opzioni[this.randomNum].name;
+                })
+                .catch(error => {
+                    console.log("Errore nell'inviare i dati: ", error);
+                })
         },
         // getData
         getData() {
             axios.get(this.api)
-            .then(response => {
-                console.log("La risposta dell'api delle capitali è: " + response);
-                this.database = {database: response.data};
+                .then(response => {
+                    console.log("La risposta dell'api delle capitali è: " + response);
+                    this.database = { database: response.data };
 
-                // sendData
-                this.sendData();
-                
-            })
-            .catch(error => {
-                console.log('Errore nel recuperare i dati: ', error);
-            })
+                    // sendData
+                    this.sendData();
+
+                })
+                .catch(error => {
+                    console.log('Errore nel recuperare i dati: ', error);
+                })
         },
         checkAnswer(i) {
             this.check = true;
             setTimeout(() => {
                 this.questionsNum--;
                 if (i == this.randomNum) {
-                    this.correctAnswers++; 
-                }   
-                
+                    this.correctAnswers++;
+                }
+
                 if (this.questionsNum > 0) {
                     // sendData
                     this.sendData();
-                } 
+                }
                 this.check = false;
             }, 2000);
         },
